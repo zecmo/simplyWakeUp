d74fa8c5741ee18fbe15cc4fc6f8366556301a04
zecmo <zecmo@asquaredgames.com>
2015-10-29 00:15:36 -0700

    Initial Commit

---
+++ simplyWakeUpSimply/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simplyWakeUpSimply
+//
+//  Created by myComputer on 10/29/15.
+//  Copyright © 2015 presentPhase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ simplyWakeUpSimply/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  simplyWakeUpSimply
+//
+//  Created by myComputer on 10/29/15.
+//  Copyright © 2015 presentPhase. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
